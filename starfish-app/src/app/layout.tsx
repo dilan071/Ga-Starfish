ad5e9db9943b014549db07c55113f7387ed5f624
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-04 16:41:57 -0500

    Update layout.tsx

--- starfish-app/src/app/layout.tsx
+++ starfish-app/src/app/layout.tsx
@@ -6,7 +6,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <body>
-        <SidebarToggle hideOnPaths={['/login','/register','/recover','/error404','/','/members', '/dashboard', '/create-retrospective', '/retrospective-list', '/retrospective-session']}>
+        <SidebarToggle hideOnPaths={['/login','/register','/error404','/'
+          ,'/members', '/dashboard', '/create-retrospective', '/retrospective-list'
+          , '/retrospective','/less-of/','/more-of/','/keep-doing/','/start-doing/','/stop-doing/','/recover']}>
           {children}
         </SidebarToggle>
       </body>
